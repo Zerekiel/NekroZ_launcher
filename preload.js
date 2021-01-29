e6194ba8c4a7837caa53c24ff3cf5dec9857f8be
Zerekiel <clement.deprost@epitech.eu>
2021-01-29 21:27:36 +0100

    [ISSUE] titlebar style messing with global window css

--- preload.js
+++ preload.js
@@ -1,7 +1,17 @@
 const customTitlebar = require('custom-electron-titlebar');
 
-window.addEventListener('DOMContentLoaded', () => {
+//window.addEventListener('readystatechange', () => {
+window.onload = () => {
   new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex('#2f3241'),
+    backgroundColor: customTitlebar.Color.fromHex('#25294A')
   });
-})
+
+  /*const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }*/
+} //)
